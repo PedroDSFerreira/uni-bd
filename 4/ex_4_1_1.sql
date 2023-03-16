a2c9f03fa5c8d72faa9e88810ccfa4ca59cc11d7
Pedro Ferreira <pedrodsf21@gmail.com>
2023-03-16 16:01:38 +0000

    Ex. 4.1.1

---
+++ 4/ex_4_1_1.sql
@@ -0,0 +1,55 @@
+CREATE TABLE cliente(
+	[nif] [int] NOT NULL PRIMARY KEY,
+	[nome] [varchar](256) NOT NULL,
+	[endereco] [varchar](256) NOT NULL,
+	[num_carta] [int] NOT NULL,
+
+);
+
+CREATE TABLE balcao(
+	[numero] [int] NOT NULL PRIMARY KEY,
+	[nome] [varchar](256) NOT NULL,
+	[endereco] [varchar](256) NOT NULL,
+);
+
+CREATE TABLE tipo_veiculo(
+	[codigo] [int] NOT NULL PRIMARY KEY,
+	[designacao] [varchar](128) NOT NULL,
+	[arcondicionado] [bit] NOT NULL,
+);
+
+CREATE TABLE veiculo(
+	[matricula] [varchar](16) NOT NULL PRIMARY KEY,
+	[ano] [int] NOT NULL,
+	[marca] [varchar](256) NOT NULL,
+	[codigo_tipo_veiculo] [int] NOT NULL REFERENCES tipo_veiculo([codigo]),
+);
+
+CREATE TABLE aluguer(
+	[numero] [int] NOT NULL PRIMARY KEY,
+	[duracao] [int] NOT NULL,
+	[data] [char](16) NOT NULL,
+	[numero_balcao] [int] NOT NULL,
+	[nif_cliente] [int] NOT NULL REFERENCES cliente([nif]),
+	[matricula_veiculo] [varchar](16) NOT NULL,
+);
+
+
+
+CREATE TABLE similaridade(
+	[codigo_tipo_veiculo_1] [int] NOT NULL REFERENCES tipo_veiculo([codigo]),
+	[codigo_tipo_veiculo_2] [int] NOT NULL REFERENCES tipo_veiculo([codigo]),
+);
+
+CREATE TABLE ligeiro(
+	[codigo_tipo_veiculo] [int] NOT NULL REFERENCES tipo_veiculo([codigo]),
+	[num_lugares] [int] NOT NULL,
+	[portas] [int] NOT NULL,
+	[combustivel] [varchar](16) NOT NULL,
+);
+
+CREATE TABLE pesado(
+	[codigo_tipo_veiculo] [int] NOT NULL REFERENCES tipo_veiculo([codigo]),
+	[peso] [int] NOT NULL,
+	[passageiros] [int] NOT NULL,
+);
