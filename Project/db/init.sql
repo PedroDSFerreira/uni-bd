9ebe7eb979d499a0ade826b3d9a65c58922612cc
Pedro Ferreira <pedrodsf21@gmail.com>
2023-05-28 22:18:48 +0100

    Update ListTasks procedure

--- Project/db/init.sql
+++ Project/db/init.sql
@@ -297,11 +297,12 @@ BEGIN
     IF @is_public = 1
     BEGIN
         SELECT t.id AS task_id, t.name AS task_name, c.name AS class_name, a.description, a.[group], a.[status], a.start_date, a.end_date, a.priority_lvl
-        FROM uni_tasks.task t
-        INNER JOIN uni_tasks.class c ON t.cl_id = c.id
-        LEFT JOIN uni_tasks.attributes a ON t.id = a.t_id AND a.is_public = 1
-        WHERE t.cl_id IN (SELECT cl_id FROM uni_tasks.enrolled_in WHERE usr_id = @usr_id)
-        ORDER BY c.name;
+		FROM uni_tasks.task t
+		INNER JOIN uni_tasks.class c ON t.cl_id = c.id
+		LEFT JOIN uni_tasks.attributes a ON t.id = a.t_id AND a.is_public = 1
+		WHERE t.cl_id IN (SELECT cl_id FROM uni_tasks.enrolled_in WHERE usr_id = @usr_id)
+			AND a.is_public = 1
+		ORDER BY c.name;
     END
     ELSE
     BEGIN
