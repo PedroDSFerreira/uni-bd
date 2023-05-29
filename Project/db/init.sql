1835c5e78818ec91efae51708e76e6282341e90a
Pedro Ferreira <pedrodsf21@gmail.com>
2023-05-29 03:25:49 +0100

    Update ListTasks procedure

--- Project/db/init.sql
+++ Project/db/init.sql
@@ -299,22 +299,46 @@ AS
 BEGIN
     IF @is_public = 1
     BEGIN
-        SELECT t.id AS task_id, t.name AS task_name, c.name AS class_name, a.description, a.[group], a.[status], a.start_date, a.end_date, a.priority_lvl
-		FROM uni_tasks.task t
-		INNER JOIN uni_tasks.class c ON t.cl_id = c.id
-		LEFT JOIN uni_tasks.attributes a ON t.id = a.t_id AND a.is_public = 1
-		WHERE t.cl_id IN (SELECT cl_id FROM uni_tasks.enrolled_in WHERE usr_id = @usr_id)
+		SELECT
+			t.id AS task_id,
+			t.name AS task_name,
+			c.name AS class_name,
+			a.description,
+			a.[group],
+			a.[status],
+			a.start_date,
+			a.end_date,
+			a.priority_lvl
+		FROM
+			uni_tasks.task t
+			INNER JOIN uni_tasks.attributes a ON a.t_id = t.id
+			INNER JOIN uni_tasks.class c ON t.cl_id = c.id
+			INNER JOIN uni_tasks.assigned_to assigned ON t.id = assigned.t_id
+		WHERE
+			assigned.usr_id = @usr_id
+			AND t.is_deleted = 0
 			AND a.is_public = 1
-		ORDER BY c.name;
     END
     ELSE
     BEGIN
-        SELECT t.id AS task_id, t.name AS task_name, c.name AS class_name, a.description, a.[group], a.[status], a.start_date, a.end_date, a.priority_lvl
-        FROM uni_tasks.task t
-        INNER JOIN uni_tasks.class c ON t.cl_id = c.id
-        LEFT JOIN uni_tasks.attributes a ON t.id = a.t_id
-        WHERE t.cl_id IN (SELECT cl_id FROM uni_tasks.enrolled_in WHERE usr_id = @usr_id)
-        ORDER BY c.name;
+		SELECT
+			t.id AS task_id,
+			t.name AS task_name,
+			c.name AS class_name,
+			a.description,
+			a.[group],
+			a.[status],
+			a.start_date,
+			a.end_date,
+			a.priority_lvl
+		FROM
+			uni_tasks.task t
+			INNER JOIN uni_tasks.attributes a ON a.t_id = t.id
+			INNER JOIN uni_tasks.class c ON t.cl_id = c.id
+			INNER JOIN uni_tasks.assigned_to assigned ON t.id = assigned.t_id
+		WHERE
+			assigned.usr_id = @usr_id
+			AND t.is_deleted = 0
     END
 END
 GO
