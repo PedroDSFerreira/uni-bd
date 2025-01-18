2588e5710e21cb9ab42fce76397d937307af5690
Pedro Ferreira <pedrodsf21@gmail.com>
2025-01-18 14:03:57 +0000

    Fix db connection

--- Project/project/db/db-init.sh
+++ Project/project/db/db-init.sh
@@ -3,11 +3,11 @@ sleep 60s
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i schema.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i tables.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i inserts.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i triggers.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i indexes.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i udfs.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i stored-procedures.sql
-/opt/mssql-tools/bin/sqlcmd -S bd -U sa -P MyPass@word -d master -i views.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i schema.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i tables.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i inserts.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i triggers.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i indexes.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i udfs.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i stored-procedures.sql
+/opt/mssql-tools18/bin/sqlcmd -S bd -U sa -P MyPass@word -C -d master -i views.sql
